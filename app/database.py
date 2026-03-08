adcfeb35ea8602a89e17737e155b284a77de8114
Nehil Bhakta <68721436+nehilbhakta@users.noreply.github.com>
2026-03-08 15:19:26 -0700

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Using SQLite for local development
+DATABASE_URL = "sqlite:///./kyobee.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},  # Needed only for SQLite
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
